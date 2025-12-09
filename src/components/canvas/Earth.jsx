3785b133e558c2b9eaf647ebbb73ba349e557dff
eerzagashiii <ereza.gashi02@gmail.com>
2025-12-09 16:13:41 +0100

    Fix import: Loader instead of loader

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -3,7 +3,7 @@ import {Canvas} from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF  } from '@react-three/drei';
 
 
-import CanvasLoader from '../loader';
+import CanvasLoader from '../Loader';
 
 
 const Earth = () => {
